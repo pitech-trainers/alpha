e29ed3e13344d3375487de0964d66f1ea90dfffe
Orban Florin <forban@pitechnologies.ro>
2013-09-25 15:31:33 +0300

    Initial commit.

---
+++ bookshop/src/Bookshop/BookshopBundle/Controller/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bookshop\BookshopBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+    return $this->render('BookshopBookshopBundle:Page:index.html.twig');
+    }
+}
