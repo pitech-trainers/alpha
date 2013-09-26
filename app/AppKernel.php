a11bfa9925d03f916bcf7a0105e6c954506a6aab
Orban Florin <forban@pitechnologies.ro>
2013-09-26 10:57:41 +0300

    Added menubundle + menu builder for header menu (guest and user).

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Bookshop\BookshopBundle\BookshopBookshopBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
