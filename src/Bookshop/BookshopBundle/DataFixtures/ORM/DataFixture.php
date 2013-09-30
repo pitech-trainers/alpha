22aa0678a026b57c8a7a30e53d52edaa0f47ea40
Orban Florin <forban@pitechnologies.ro>
2013-09-30 16:11:03 +0300

    Added finder service + relative path in DataFixture loader.

--- src/Bookshop/BookshopBundle/DataFixtures/ORM/DataFixture.php
+++ src/Bookshop/BookshopBundle/DataFixtures/ORM/DataFixture.php
@@ -6,11 +6,13 @@ use Hautelook\AliceBundle\Alice\DataFixtureLoader;
 use Nelmio\Alice\Fixtures;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Finder\Finder;
+use Symfony\Component\DependencyInjection\ContainerAware;
 
-class DataLoader implements FixtureInterface
+
+
+class DataLoader  extends ContainerAware implements FixtureInterface
 {
-    protected static $path = "/home/forban/Sites/alpha/src/Bookshop/BookshopBundle/Resources/config/Fixtures/Data.yml";
+//    protected static $path = "/home/forban/Sites/alpha/src/Bookshop/BookshopBundle/Resources/config/Fixtures/Data.yml";
    
     public function load(ObjectManager $manager)
     {
@@ -18,7 +20,14 @@ class DataLoader implements FixtureInterface
         $faker = new \Faker\Generator();
         $fakerProvider = new \Bookshop\BookshopBundle\Faker\Provider\DataProvider($faker);
         $faker->addProvider($fakerProvider);
-        Fixtures::load(static::$path, $manager, array('providers' => array($fakerProvider)));
+        $finder = $this->container->get("finder");
+        $finder->files()->name('Data.yml')->in('src/Bookshop/BookshopBundle/Resources');     
+
+        foreach ($finder as $file) {
+            $path = $file->getRealpath();
+        }
+
+        Fixtures::load($path, $manager, array('providers' => array($fakerProvider)));
               
     }
 }
