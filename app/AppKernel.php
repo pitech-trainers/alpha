f65c7071d6c09f778dc60bf9b34b955610022d26
Orban Florin <forban@pitechnologies.ro>
2013-09-30 17:12:26 +0300

    Started work on categoryPage.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Hautelook\AliceBundle\HautelookAliceBundle()
+            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
